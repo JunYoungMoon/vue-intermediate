826c87c8ac6bf312aa15699ef508401fbe2461d7
Jun <moderato33@entin.co.kr>
2021-11-12 11:25:26 +0000

    props, event 방식 추가

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,5 +1,7 @@
 <template>
 	<div id="app">
+		<!-- <Event v-on:sendevent="receiveEvent"></Event> -->
+		<!-- <Props v-bind:receivedata="propsdata"></Props> -->
 		<TodoHeader></TodoHeader>
 		<TodoInput></TodoInput>
 		<TodoList></TodoList>
@@ -8,17 +10,30 @@
 </template>
 
 <script>
+//import Event from './components/Event.vue';
+//import Props from './components/Props.vue';
+	
 import TodoHeader from './components/TodoHeader.vue'
 import TodoInput from './components/TodoInput.vue'
 import TodoList from './components/TodoList.vue'
 import TodoFooter from './components/TodoFooter.vue'
 
 export default {
+	data:function(){
+		return{
+			//propsdata : '데이터 내리기'
+		}
+	},
+	methods:{
+		//receiveEvent:function(){alert('이벤트 올려받기')}
+	},
 	components: {
-	  TodoHeader: TodoHeader,
-	  TodoInput: TodoInput,
-	  TodoList: TodoList,
-	  TodoFooter: TodoFooter
+		//Event, Props,
+		TodoHeader: TodoHeader,
+		TodoInput: TodoInput,
+		TodoList: TodoList,
+		TodoFooter: TodoFooter,
+		
 	}
 }
 </script>
